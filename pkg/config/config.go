fb1a4c3e6710d2548af97902b7f60c427b8c0574
Felipe Silva <carlosfelipesilva.fs@gmail.com>
2022-11-06 14:21:17 -0300

    refactor: refactoring database connection to use env

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,8 +2,10 @@ package config
 
 import (
 	"fmt"
+	"os"
 
 	entities "github.com/felpssc/api-golang/internal/modules/users/entities/user"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -11,7 +13,21 @@ import (
 var db *gorm.DB
 
 func Connect() {
-	dsn := "host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable"
+
+	err := godotenv.Load()
+
+	if err != nil {
+		panic("Failed to load .env file")
+	}
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USERNAME"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_DATABASE"),
+		os.Getenv("DB_PORT"),
+	)
+
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {

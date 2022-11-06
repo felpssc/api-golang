988b479bbe83567e66eab9694cbfdf3469173886
Felipe Silva <carlosfelipesilva.fs@gmail.com>
2022-11-06 14:22:41 -0300

    test: add create user unitary tests

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.18
 require github.com/labstack/echo/v4 v4.7.2
 
 require (
+	github.com/bxcodec/faker/v3 v3.8.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.13.0 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -14,6 +16,8 @@ require (
 	github.com/jackc/pgtype v1.12.0 // indirect
 	github.com/jackc/pgx/v4 v4.17.2 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 require (
@@ -22,6 +26,7 @@ require (
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/stretchr/testify v1.8.1
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	golang.org/x/crypto v0.1.0 // indirect
---
+++ internal/modules/users/useCases/createUser/createUserUseCase_test.go
@@ -0,0 +1,56 @@
+package usecase
+
+import (
+	"testing"
+
+	"github.com/bxcodec/faker/v3"
+	user "github.com/felpssc/api-golang/internal/modules/users/entities/user"
+	repository "github.com/felpssc/api-golang/internal/modules/users/repositories/usersRepository/implementations"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateUser(t *testing.T) {
+	usersRepository := repository.NewLocalUsersRepository()
+
+	createUserUseCase := NewCreateUserUseCase(usersRepository)
+
+	user := user.User{
+		Name:     faker.Name(),
+		Email:    faker.Email(),
+		Password: faker.Password(),
+	}
+
+	createdUser, err := createUserUseCase.execute(user.Name, user.Email, user.Password)
+
+	assert.Nil(t, err)
+
+	assert.Equal(t, user.Name, createdUser.Name)
+
+	assert.NotEqual(t, 0, createdUser.ID)
+}
+
+func TestIfEmailExists(t *testing.T) {
+	usersRepository := repository.NewLocalUsersRepository()
+
+	createUserUseCase := NewCreateUserUseCase(usersRepository)
+
+	user := user.User{
+		Name:     faker.Name(),
+		Email:    faker.Email(),
+		Password: faker.Password(),
+	}
+
+	createdUser, err := createUserUseCase.execute(user.Name, user.Email, user.Password)
+
+	assert.Nil(t, err)
+
+	assert.Equal(t, user.Name, createdUser.Name)
+
+	assert.NotEqual(t, 0, createdUser.ID)
+
+	_, err = createUserUseCase.execute(user.Name, user.Email, user.Password)
+
+	assert.NotNil(t, err)
+
+	assert.Equal(t, "user already exists", err.Error())
+}
